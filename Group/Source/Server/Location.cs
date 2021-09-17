722eb5a95e830b6829f2f305c465d74667ec7d27
santiagoflorez98 <santiagoo98p@gmail.com>
2021-09-17 17:35:40 -0300

    Location class added

---
+++ Group/Source/Server/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Location
+{
+    private int id;
+    private string coordinates;
+    public int PostalCode { get; set; }
+    public string Address { get; set; }
+    public string Floor { get; set; }
+    public string Number { get; set; }
+    public LocationType Type { get; set; }
+    public enum LocationType 
+    {warehouse=0,
+    mailOffice=1,
+    endPoint=2}
+}
