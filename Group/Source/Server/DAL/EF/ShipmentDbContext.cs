365b8b9f712c38c29b69ead99dc843f478f8034b
santiagoflorez98 <santiagoo98p@gmail.com>
2021-09-24 10:46:56 -0300

    [Corrections] SystemDbContext is now ShipmentDbContext some DbSets were eliminated from ShipmentDbContext

--- Group/Source/Server/DAL/EF/ShipmentDbContext.cs
+++ Group/Source/Server/DAL/EF/ShipmentDbContext.cs
@@ -6,18 +6,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Server.DAL.EF
 {
-    public class ProgramDbContext : DbContext
+    public class ShipmentDbContext : DbContext
     {
         public DbSet<Shipment> Shipments { get; set; }
         public DbSet<Itinerary> Itineraries { get; set; }
         public DbSet<Leg> Legs { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<Location> Locations { get; set; }
-        public DbSet<ProfileInfo> Profiles { get; set; }
         protected override void OnModelCreating(ModelBuilder pModelBuilder)
         {
-            pModelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
-            base.OnModelCreating(pModelBuilder);
         }
     }
 }
