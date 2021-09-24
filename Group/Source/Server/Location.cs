1f3f39991d0bd0b8e8a7aaaf03051cc427ac5f4a
enzodprediger <enzodprediger@gmail.com>
2021-09-24 10:07:59 -0300

    Corrections

--- Group/Source/Server/Location.cs
+++ Group/Source/Server/Location.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 
-public class Location
+
+namespace Server
 {
-    private int id;
-    private string coordinates;
-    public int PostalCode { get; set; }
-    public string Address { get; set; }
-    public string Floor { get; set; }
-    public string Number { get; set; }
-    public LocationType Type { get; set; }
-    public enum LocationType 
-    {warehouse=0,
-    mailOffice=1,
-    endPoint=2}
+    public class Location
+    {
+        private int id;
+        private string coordinates;
+        public int PostalCode { get; set; }
+        public string Address { get; set; }
+        public string Floor { get; set; }
+        public string Number { get; set; }
+        public LocationType Type { get; set; }
+
+    }
 }
+
