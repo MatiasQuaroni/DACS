6883bc5cf7d74c0099f2a3fc4896c8991505bb6a
santiagoflorez98 <santiagoo98p@gmail.com>
2021-09-24 11:13:58 -0300

    [Create] ShipmentEntityConfiguration class was created

---
+++ Group/Source/Server/DAL/EF/ShipmentEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Server.DAL.EF
+{
+    public class ShipmentEntityConfiguration : EntityTypeConfiguration<Shipment>
+    {
+        public ShipmentEntityConfiguration()
+        {
+            this.ToTable("ShipmentInfo");
+            this.HasKey<Guid>(s => s.Id);
+        }
+    }
+}
