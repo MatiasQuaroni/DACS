bd5a7e0d075fff12f9fcf3c55d9a3d4bb22ee622
enzodprediger <enzodprediger@gmail.com>
2021-09-24 10:48:09 -0300

    UserDbContext added

---
+++ Group/Source/Server/DAL/EF/UserDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.DAL
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProfileInfo> Profiles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>().ToTable("UserInfo")
+                                       .HasKey(u => u.Id);
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.UserName)
+                .HasColumnName("Username")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.Password)
+                .HasMaxLength(50)
+                .HasColumnName("Password");
+
+            modelBuilder.Entity<UserState>()
+                .Property(p => p.Status)
+                .HasColumnName("Status");
+
+            modelBuilder.Entity<ProfileInfo>()
+                .Property(p => p.DisplayName)
+                .HasColumnName("Displayname");
+
+            modelBuilder.Entity<ProfileInfo>()
+                .Property(p => p.EmailAddress)
+                .HasColumnName("Email");
+
+            modelBuilder.Entity<ProfileInfo>()
+                .Property(p => p.PhoneNumber)
+                .HasColumnName("Phone number");
+
+        }
+    }
+}
