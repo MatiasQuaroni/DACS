1bb8b951623ba9cd73ef6aa3d5c2e33762f624e2
santiagoflorez98 <santiagoo98p@gmail.com>
2021-10-15 19:07:18 -0300

    [Feat] ItineraryDTO and LegDTO classes created

---
+++ Group/Source/Server/Application/Services/DTOs/ItineraryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Application.Services.DTOs
+{
+    public class ItineraryDTO
+    {
+        public Guid Id { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public IEnumerable<ShipmentDTO> Shipments { get; set; }
+        public IEnumerable<LegDTO> Legs { get; set; }
+    }
+}
---
+++ Group/Source/Server/Application/Services/DTOs/LegDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Application.Services.DTOs
+{
+    public class LegDTO
+    {
+        public Guid Id { get; set; }
+        public LocationDTO StartLocation { get; set; }
+        public LocationDTO EndLocation { get; set; }
+    }
+}
