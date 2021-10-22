aeafecacc270eff50967ad47eb4b741d6c0157e2
Lucas Agustin Neuwit Quaroni <lquaroni@itsynch.com>
2021-10-21 21:48:29 -0300

    feat: model, initial state and reducer

---
+++ Group/Source/Client/src/app/shipments/+state/model.ts
@@ -0,0 +1,41 @@
+export interface Shipment {
+  id: string;
+  trackingNumber: string;
+  weight: number;
+  precautions: string;
+  arrivalDate: Date;
+  estimatedArrivalDate: Date;
+  destinationAddressId: string;
+  customerInfoId: string;
+}
+
+export interface Location {
+  id: string;
+  postalCode: string;
+  coordinates: string;
+  address: string;
+  floor: number;
+  number: number;
+}
+
+export interface CustomerInfo {
+  id: string;
+  name: string;
+  emailAddress: string;
+  dni: string;
+  phoneNumber: string;
+}
+
+export interface Itinerary {
+  id: string;
+  isComplete: boolean;
+  startDate: Date;
+  endDate?: Date;
+  legs: string[];
+}
+
+export interface Leg {
+  id: string;
+  startLocationId: string;
+  endLocationId: string;
+}
