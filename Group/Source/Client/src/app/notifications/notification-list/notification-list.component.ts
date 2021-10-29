d6ea05e019b45391252295d30e32c7c7007eda29
Matias <matiasquaroni98@gmail.com>
2021-10-29 19:54:23 -0300

    [feat] notification-list component

---
+++ Group/Source/Client/src/app/notifications/notification-list/notification-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Notification } from '../+state/model';
+
+@Component({
+  selector: 'notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss'],
+})
+export class NotificationListComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  public notifications: Notification[];
+
+  public selectedNotifications: string[];
+  
+
+  ngOnInit() {}
+
+}
