c5178feb85e5460cba6e8f5fd74c9804dd2f9c51
Matias <matiasquaroni98@gmail.com>
2021-10-29 20:40:35 -0300

    [feat] tracking-tool component

---
+++ Group/Source/Client/src/app/shipments/tracking-tool/tracking-tool.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracking-tool',
+  templateUrl: './tracking-tool.component.html',
+  styleUrls: ['./tracking-tool.component.scss'],
+})
+export class TrackingToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
