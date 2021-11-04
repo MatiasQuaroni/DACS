19b2925eee5659e4ba9d5bf2bc46b5d35e291808
Matias <matiasquaroni98@gmail.com>
2021-11-03 22:03:20 -0300

    [feat] tracking-tool component

--- Group/Source/Client/src/app/home/home.module.ts
+++ Group/Source/Client/src/app/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
 import { HomePage } from './home-page/home.page';
 import { LayoutModule } from '../layout/layout.module';
 import { HomeComponent } from './home-component/home.component';
 import { MatBadgeModule } from '@angular/material/badge';
+import { ReactiveFormsModule} from '@angular/forms'
+import {MatStepperModule} from '@angular/material/stepper'
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule,} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -18,7 +20,13 @@ import { MatBadgeModule } from '@angular/material/badge';
     IonicModule,
     HomePageRoutingModule,
     LayoutModule,
-    MatBadgeModule
+    MatBadgeModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+
   ],
   declarations: [HomePage, HomeComponent],
   exports: [HomeComponent]
--- Group/Source/Client/src/app/shipments/tracking-tool/tracking-tool.component.ts
+++ Group/Source/Client/src/app/shipments/tracking-tool/tracking-tool.component.ts
@@ -1,14 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ShipmentState } from '../+state/model';
+
+const shipmentStates : ShipmentState[] = [
+  {
+    id: '1',
+    currentState: 'In Preparation',
+    fromDate: new Date(),
+    toDate: new Date(Date.now()+ 1 *24*60*60*1000),
+  },
+  {
+    id: '2',
+    currentState: 'Ongoing',
+    fromDate: new Date(Date.now()+ 1 *24*60*60*1000),
+    toDate: new Date(Date.now()+ 2 *24*60*60*1000),
+  },
+  {
+    id: '3',
+    currentState: 'Ongoing',
+    fromDate: new Date(Date.now()+ 2 *24*60*60*1000),
+    toDate: new Date(Date.now()+ 3 *24*60*60*1000),
+  },
+  {
+    id: '4',
+    currentState: 'Delivered',
+    fromDate: new Date(Date.now()+ 3 *24*60*60*1000),
+    toDate: new Date(Date.now()+ 3 *24*60*60*1000),
+  }
+
+]
 
 @Component({
-  selector: 'app-tracking-tool',
+  selector: 'tracking-tool',
   templateUrl: './tracking-tool.component.html',
   styleUrls: ['./tracking-tool.component.scss'],
 })
 export class TrackingToolComponent implements OnInit {
+isLinear = false;
+firstFormGroup : FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder : FormBuilder) {}
 
-  ngOnInit() {}
+  @Input()
+  public shipmentStates$ = shipmentStates; // Note: this should be retrieved from the store and typed as Observable<ShipmentState[]>, using dummy placeholder for the moment.
+
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group ({
+      trackingID: ['', Validators.required],
+    });
+  }
+
 
 }
