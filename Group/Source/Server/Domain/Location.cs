677502fa91b0229f12da4e3782f715660d29ff27
santiagoflorez98 <santiagoo98p@gmail.com>
2021-11-05 10:39:40 -0300

    fix: Coordinates property type is now double[] Location constructor now creates the Location Id

--- Group/Source/Server/Domain/Location.cs
+++ Group/Source/Server/Domain/Location.cs
@@ -7,12 +7,18 @@ namespace Server.Domain
     public class Location
     {
         public Guid Id { get; set; }
-        public string Coordinates { get; set; }
+        public double[] Coordinates { get; set; }
         public int PostalCode { get; set; }
         public string Address { get; set; }
         public string Floor { get; set; }
         public string Number { get; set; }
         public LocationType Type { get; set; }
+        public Shipment Shipment { get; set; }
+
+        public Location()
+        {
+            this.Id = new Guid();
+        }
     }
 }
 
