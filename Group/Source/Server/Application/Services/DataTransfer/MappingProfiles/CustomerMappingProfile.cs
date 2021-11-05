2211185867e16c1abefea6fe391f51a43f74b7ed
santiagoflorez98 <santiagoo98p@gmail.com>
2021-11-05 10:50:48 -0300

    fix: automapping from dto to entity was deleted from the mappings profiles

--- Group/Source/Server/Application/Services/DataTransfer/MappingProfiles/CustomerMappingProfile.cs
+++ Group/Source/Server/Application/Services/DataTransfer/MappingProfiles/CustomerMappingProfile.cs
@@ -16,12 +16,6 @@ namespace Server.Application.Services.DataTransfer.MappingProfiles
             CreateMap<CustomerInfo, CustomerData>().ForMember(dto => dto.Email, src => src.MapFrom(src => src.Email));
             CreateMap<CustomerInfo, CustomerData>().ForMember(dto => dto.Name, src => src.MapFrom(src => src.Name));
             CreateMap<CustomerInfo, CustomerData>().ForMember(dto => dto.PhoneNumber, src => src.MapFrom(src => src.PhoneNumber));
-
-            CreateMap<CustomerData, CustomerInfo>().ForMember(src => src.Id, dto => dto.MapFrom(dto => dto.Id));
-            CreateMap<CustomerData, CustomerInfo>().ForMember(src => src.Dni, dto => dto.MapFrom(dto => dto.Dni));
-            CreateMap<CustomerData, CustomerInfo>().ForMember(src => src.Email, dto => dto.MapFrom(dto => dto.Email));
-            CreateMap<CustomerData, CustomerInfo>().ForMember(src => src.Name, dto => dto.MapFrom(dto => dto.Name));
-            CreateMap<CustomerData, CustomerInfo>().ForMember(src => src.PhoneNumber, dto => dto.MapFrom(dto => dto.PhoneNumber));
         }
     }
 }
--- Group/Source/Server/Application/Services/DataTransfer/MappingProfiles/ShipmentStateMappingProfile.cs
+++ Group/Source/Server/Application/Services/DataTransfer/MappingProfiles/ShipmentStateMappingProfile.cs
@@ -14,10 +14,6 @@ namespace Server.Application.Services.DataTransfer.MappingProfiles
             CreateMap<ShipmentState, ShipmentStateData>().ForMember(dto => dto.CurrentState, src => src.MapFrom(src => src.CurrentState));
             CreateMap<ShipmentState, ShipmentStateData>().ForMember(dto => dto.FromDate, src => src.MapFrom(src => src.FromDate));
             CreateMap<ShipmentState, ShipmentStateData>().ForMember(dto => dto.ToDate, src => src.MapFrom(src => src.ToDate));
-
-            CreateMap<ShipmentStateData, ShipmentState>().ForMember(dto => dto.CurrentState, src => src.MapFrom(src => src.CurrentState));
-            CreateMap<ShipmentStateData, ShipmentState>().ForMember(dto => dto.FromDate, src => src.MapFrom(src => src.FromDate));
-            CreateMap<ShipmentStateData, ShipmentState>().ForMember(dto => dto.ToDate, src => src.MapFrom(src => src.ToDate));
         }
     }
 }
