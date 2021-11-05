215116bd1ba85548810c2fdd1fac5d6da377f4fc
santiagoflorez98 <santiagoo98p@gmail.com>
2021-11-05 10:17:15 -0300

    Relationships between Shipment, CustomerInfo and Location was added. Location now has a double to string and a string to double converter

--- Group/Source/Server/Persistence/ClassMappings/LocationEntityConfiguration.cs
+++ Group/Source/Server/Persistence/ClassMappings/LocationEntityConfiguration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Domain;
+using System;
+using System.Linq;
 
 namespace Server.Persistence.ClassMappings
 {
@@ -8,13 +11,18 @@ namespace Server.Persistence.ClassMappings
     {
         public void Configure(EntityTypeBuilder<Location> builder)
         {
+            var converter = new ValueConverter<double[], string>(
+                v => string.Join("/", v),
+                v => v.Split("/", StringSplitOptions.RemoveEmptyEntries).Select(val => double.Parse(val)).ToArray());
+
             builder.HasKey(l => l.Id);
             builder.Property(l => l.Address);
             builder.Property(l => l.Floor);
             builder.Property(l => l.Number);
             builder.Property(l => l.PostalCode);
-            builder.Property(l => l.Coordinates);
+            builder.Property(l => l.Coordinates).HasConversion(converter); ;
             builder.Property(l => l.Type);
+            builder.HasOne<Shipment>(l => l.Shipment).WithOne(s => s.DestinationAddress).HasForeignKey<Shipment>(da => da.DestinationAddressId);
         }
     }
 }
