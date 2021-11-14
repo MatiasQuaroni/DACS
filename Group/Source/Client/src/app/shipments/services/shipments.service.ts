a169bfb2f5cc67a006c9c9fe01f0026ce2c7bd24
Matias <matiasquaroni98@gmail.com>
2021-11-13 21:50:41 -0300

    [feat] shipments effects

--- Group/Source/Client/src/app/shipments/services/shipments.service.ts
+++ Group/Source/Client/src/app/shipments/services/shipments.service.ts
@@ -1,14 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Shipment } from '../+state/model';
+import { CustomerInfo, Itinerary, Shipment, Leg, Location } from '../+state/model';
 
+export const ROADS_SERVER_URL = ''
 @Injectable({
   providedIn: 'root',
 })
 export class ShipmentsService {
+  createItinerary(shipmentIds: string[]): Observable<Itinerary> {
+    this.client.post(ROADS_SERVER_URL, shipmentIds).subscribe((itinerary) =>{
+      return itinerary
+    }
+  }
+  getAllLegs(): Observable<Leg[]> {
+    throw new Error('Method not implemented.');
+  }
+  getAllCustomers(): Observable<CustomerInfo[]> {
+    throw new Error('Method not implemented.');
+  }
+  getAllItineraries(): Observable<Itinerary[]> {
+    throw new Error('Method not implemented.');
+  }
+  getAllLocations(): Observable<Location[]> {
+    throw new Error('Method not implemented.');
+  }
   getAllShipments(): Observable<Shipment[]> {
-    return this.client.get('');
+    throw new Error('Method not implemented.');
   }
   constructor(private client: HttpClient) {}
 }
