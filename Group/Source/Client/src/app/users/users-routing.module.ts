7cfb89c162a917be11248f69405bc512a881186d
Lucas Agustin Neuwit Quaroni <lquaroni@itsynch.com>
2021-11-29 23:24:34 -0300

    update: users module

--- Group/Source/Client/src/app/users/users-routing.module.ts
+++ Group/Source/Client/src/app/users/users-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./signup/signup.module').then((m) => m.SignupPageModule),
   },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+  },
+
 ];
 
 @NgModule({
--- Group/Source/Client/src/app/users/users.module.ts
+++ Group/Source/Client/src/app/users/users.module.ts
@@ -6,12 +6,20 @@ import { StoreModule } from '@ngrx/store';
 import * as fromUsers from './+state/reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { UsersEffects } from './+state/effects';
+import { LoginPageModule } from './login/login.module';
+import { SignupPageModule } from './signup/signup.module';
+import { UsersRoutingModule } from './users-routing.module';
+import { AccountPageModule } from './account/account.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     IonicModule,
+    LoginPageModule,
+    SignupPageModule,
+    UsersRoutingModule,
+    AccountPageModule,
     StoreModule.forFeature(fromUsers.usersFeatureKey, fromUsers.reducer),
     EffectsModule.forFeature([UsersEffects]),
   ],
